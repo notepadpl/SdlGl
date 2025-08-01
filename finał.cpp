e6288eeca243d4b766efa860a09eb9bfca9b45f5
notepadpl <przychodzien12@gmail.com>
2025-08-01 23:22:16 +0200

    Update finał.cpp

--- finał.cpp
+++ finał.cpp
@@ -212,6 +212,9 @@ bool init() {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
         stbi_image_free(data);
     } else {
         printf("No materials found, no texture loaded.\n");
