2d0581a28b4841ad84f6378692ca85fb5d58a538
notepadpl <notepadpl@users.noreply.github.com>
2025-08-02 07:15:58 +0000

    deploy: 4fa0a9f05362fe2d0aa6e453109a292ad45f9a56

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp3ajgp_xc.js
+// include: /tmp/tmp987_7x26.js
 
   if (!Module['expectedDataFileDownloads']) {
     Module['expectedDataFileDownloads'] = 0;
@@ -220,21 +220,21 @@ Module['FS_createPath']("/", "asserts", true, true);
 
   })();
 
-// end include: /tmp/tmp3ajgp_xc.js
-// include: /tmp/tmpd5t9vazi.js
+// end include: /tmp/tmp987_7x26.js
+// include: /tmp/tmpu8axdkdy.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmpd5t9vazi.js
-// include: /tmp/tmpbc1zviyw.js
+  // end include: /tmp/tmpu8axdkdy.js
+// include: /tmp/tmptp33mtah.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpbc1zviyw.js
+  // end include: /tmp/tmptp33mtah.js
 
 
 // Sometimes an existing Module object exists with properties
