3ec0ebb111caedcf09935e143018ebc98dc6e523
notepadpl <przychodzien12@gmail.com>
2025-08-14 14:54:19 +0200

    Update logic.cpp

--- logic.cpp
+++ logic.cpp
@@ -10,7 +10,7 @@
 #include <string>
 #include <cmath>
 #include <iostream>
-
+#define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
